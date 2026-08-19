a27470df394fa27661039adee2f9d6f32ff199b6
Marcos Rivera <marcos.rivera@knowmadmood.com>
2026-08-19 09:45:31 +0200

    feat: estructura de paginas con about (por defecto), contact y pricing

---
+++ src/app/pages/about/about-page.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './about-page.html',
+})
+export default class AboutPage {}
---
+++ src/app/pages/pricing/pricing-page.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './pricing-page.html',
+})
+export default class PricingPage {}
